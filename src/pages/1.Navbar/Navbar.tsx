dc457cb95fcee7b939d61230319a3cb8d48a4e6a
kemboi590 <bkemboi590@gmail.com>
2024-06-23 07:54:17 +0300

    completed the nav navbar

--- src/pages/1.Navbar/Navbar.tsx
+++ src/pages/1.Navbar/Navbar.tsx
@@ -32,28 +32,27 @@ export const Navbar = () => {
         </h2>
       </div>
       <div className="text-lg lg:font-bold sm:font-medium hidden md:block">
-        <a href="/" className="p-2 lg:p-4">Home</a>
-        <a href="/about" className="p-2 lg:p-4">Education</a>
-        <a href="/services" className="p-2 lg:p-4">Experience</a>
-        <a href="/projects" className="p-2 lg:p-4">Projects</a>
-        <a href="/community" className="p-2 lg:p-4">Community</a>
-        <a href="/contact" className="p-2 lg:p-4">Contact</a>
+        <a href="#home" className="p-2 lg:p-4">Home</a>
+        <a href="#about" className="p-2 lg:p-4">Education</a>
+        <a href="#services" className="p-2 lg:p-4">Experience</a>
+        <a href="#projects" className="p-2 lg:p-4">Projects</a>
+        <a href="#community" className="p-2 lg:p-4">Community</a>
+        <a href="#contact" className="p-2 lg:p-4">Contact</a>
       </div>
 
-      {/* Fix it at the right */}
+      {/* humbuger menu */}
       <div
         onClick={handleNav}
         className="block md:hidden fixed right-0 top-0 p-4 cursor-pointer"
       >
         {nav ? <AiOutlineClose size={30} /> : <AiOutlineMenu size={30} />}
       </div>
+
       {/* For small screens */}
       <div
-        className={nav
-          ? "fixed left-0 top-0 w-[60%] h-full border-r border-gray-900 bg-[#000300] ease-in-out duration-500"
-          : "ease-in-out duration-500 fixed left-[-100%]"
-        }
-      >
+        className={nav ? "fixed left-0 top-0 w-[60%] h-full border-r border-gray-900 bg-[#000300] ease-in-out duration-500"
+          : "ease-in-out duration-500 fixed left-[-100%]"}>
+
         <h2 className="w-full text-3xl font-bold text-[#00df9a] m-4">
           <a href="/">Kemboi</a>
         </h2>
