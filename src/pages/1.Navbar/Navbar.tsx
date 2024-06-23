aa4bcb69d925682da6a7788ececaeb1b55783a19
kemboi590 <bkemboi590@gmail.com>
2024-06-23 08:01:29 +0300

    navbar fix on height to full screen

--- src/pages/1.Navbar/Navbar.tsx
+++ src/pages/1.Navbar/Navbar.tsx
@@ -50,7 +50,7 @@ export const Navbar = () => {
 
       {/* For small screens */}
       <div
-        className={nav ? "fixed left-0 top-0 w-[60%] h-full border-r border-gray-900 bg-[#000300] ease-in-out duration-500"
+        className={nav ? "fixed left-0 top-0 w-[60%] h-screen border-r border-gray-900 bg-[#000300] ease-in-out duration-500"
           : "ease-in-out duration-500 fixed left-[-100%]"}>
 
         <h2 className="w-full text-3xl font-bold text-[#00df9a] m-4">
