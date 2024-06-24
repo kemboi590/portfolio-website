c293e48a836a5df727b032a5a5525b402f329142
kemboi590 <bkemboi590@gmail.com>
2024-06-24 07:33:32 +0300

    added education component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import './App.css'
 import { Navbar } from './pages/1.Navbar/Navbar'
 import { Home } from './pages/2.Home/Home'
+import { Education } from './pages/4.Education/Education'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <div className='bg-dark' >
       <Navbar />
       <Home />
+      <Education />
     </div>
   )
 }
