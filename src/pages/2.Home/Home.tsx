ba9919104d5b9be6f2d43e1bc2eadabbfdcfd31d
kemboi590 <bkemboi590@gmail.com>
2024-06-24 06:49:14 +0300

    added about me component

--- src/pages/2.Home/Home.tsx
+++ src/pages/2.Home/Home.tsx
@@ -2,38 +2,42 @@ import { ReactTyped } from "react-typed";
 import Potrait from '../../assets/images/kemboi.jpg'
 import Resume from '../../assets/files/Resume.pdf';
 import { HiDownload } from 'react-icons/hi'
+import About from "../3.About/About";
 
 
 export const Home = () => {
     return (
-        <section id="home" className="flex flex-col md:flex-row justify-center items-center h-fit py-[3.5rem] text-text-light relative" >
-            <div className="w-full md:w-1/2 flex justify-center items-center z-30">
-                <div className="px-8 pb-8">
-                    <h1 className="text-2xl md:text-4xl font-bold">Hi, I'm
-                        {/* want to have */}
-                        <span className='text-[#00df9a] ' >
-                            <ReactTyped
-                                className="pl-2"
-                                strings={['Brian Kemboi']}
-                                typeSpeed={120}
-                                backSpeed={70}
-                                loop />
-                        </span>
-                    </h1>
+        <>
+            <section id="home" className="flex flex-col md:flex-row justify-center items-center h-fit  md:pt-[3.5rem] pb-8 text-text-light relative" >
+                <div className="w-full md:w-1/2 flex justify-center items-center z-30">
+                    <div className="px-4 md:px-8 pb-8">
+                        <h1 className="text-2xl md:text-4xl font-bold">Hi, I'm
+                            {/* React Type */}
+                            <span className='text-[#00df9a] ' >
+                                <ReactTyped
+                                    className="pl-2"
+                                    strings={['Brian Kemboi']}
+                                    typeSpeed={120}
+                                    backSpeed={70}
+                                    loop />
+                            </span>
+                        </h1>
 
-                    <p className="text-xl w-full md:w-[1/2] leading-relaxed text-justify">
-                        Full-stack web developer with strong foundation in Azure cloud. As a dedicated community leader, I strive to foster collaboration and innovation within the tech community.
-                    </p>
+                        <p className="text-xl w-full md:w-[1/2] leading-relaxed text-justify">
+                            Full-stack web developer with strong foundation in Azure cloud. As a dedicated community leader, I strive to foster collaboration and innovation within the tech community.
+                        </p>
 
-                    <a href={Resume} download className="mt-4 bg-[#00df9a] p-3 rounded-lg hover:bg-[#00b48a] transition duration-500 ease-in-out flex items-center space-x-2 w-[230px] text-lg md:text-xl">
-                        <span>Download Resume</span>
-                        <HiDownload size={26}/>
-                    </a>
+                        <a href={Resume} download className="mt-4 bg-[#00df9a] p-3 rounded-lg hover:bg-[#00b48a] transition duration-500 ease-in-out flex items-center space-x-2 w-[230px] text-lg md:text-xl">
+                            <span>Download Resume</span>
+                            <HiDownload size={26} />
+                        </a>
+                    </div>
                 </div>
-            </div>
-            <div className="w-full md:w-1/2 flex justify-center items-center z-20">
-                <img src={Potrait} alt="Kemboi" className="w-60 h-60 rounded-lg transform transition duration-500 hover:scale-110" />
-            </div>
-        </section>
+                <div className="w-full md:w-1/2 flex justify-center items-center z-20">
+                    <img src={Potrait} alt="Kemboi" className="w-60 h-60 rounded-lg transform transition duration-500 hover:scale-110" />
+                </div>
+            </section>
+            <About />
+        </>
     )
 }
