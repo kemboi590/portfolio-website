2d60a851cb7cc995d478ef3cd5c9deed6e0ce5d3
kemboi590 <bkemboi590@gmail.com>
2024-06-24 12:43:41 +0300

    added content to experience component

---
+++ src/pages/5.Experience/Experience.tsx
@@ -0,0 +1,25 @@
+import { ExperienceData } from "../../components/ExperienceData"
+
+export const Experience = () => {
+    return (
+        <>
+            <h1 className='text-4xl font-bold text-[#00df9a] text-center'>Experience</h1>
+            <div className='flex flex-col p-5 md:flex-wrap md:flex-row justify-center items-center h-fit gap-4 md:gap-8 xl:gap-10 md:pt-[3.5rem] pb-8 text-text-light'>
+                {ExperienceData.map((experience) => {
+                    return (
+                        <div key={experience.id} className='flex bg-slate-700 w-full md:w-[40%]  p-4 md:p-6 cursor-pointer transform transition duration-300 hover:-translate-y-3 border-2 gap-4'>
+
+                            <div className="w-[10%] flex justify-center items-center">
+                                <img src={experience.logo} alt={experience.title} className='w-12 rounded-lg' />
+                            </div>
+                            <div className='w-[90%]'>
+                                <h5 className='text-[#00df9a] flex flex-col md:flex-row justify-between'><b>{experience.title}</b> <span className="text-text-light text-sm md:text-xs xl:text-sm">{experience.period}</span></h5>
+                                <p className='pt-2 leading-7 text-lg'>{experience.description}</p>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+        </>
+    )
+}
