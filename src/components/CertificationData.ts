1b20f2006f6798a27ffc1f48fc2e260f70ae9635
kemboi590 <bkemboi590@gmail.com>
2024-06-24 17:10:22 +0300

    added more links

--- src/components/CertificationData.ts
+++ src/components/CertificationData.ts
@@ -12,51 +12,61 @@ import AzureDBMySQLGold from '../assets/images/mySQL-Gold-Logo.png'
 
 export const CertificationData = [
     {
+        id: 1,
         certLogo: AzureAIEngineerAssociate,
         certTitle: 'Microsoft Certified: Azure AI Engineer Associate',
         certLink: 'https://learn.microsoft.com/api/credentials/share/en-us/BrianKemboi-8686/54A880168634AD47?sharingId=F3C1BA3926A133BD'
     },
     {
+        id: 2,
         certLogo: AzureFundamentals,
         certTitle: 'Microsoft Certified: Azure Fundamentals',
         certLink: 'https://learn.microsoft.com/api/credentials/share/en-us/BrianKemboi-8686/F3FA5B287417BE1B?sharingId=F3C1BA3926A133BD'
     },
     {
+        id: 3,
         certLogo: AzureAIFundamentals,
         certTitle: 'Microsoft Certified: Azure AI Fundamentals',
         certLink: 'https://learn.microsoft.com/api/credentials/share/en-us/BrianKemboi-8686/F3FA5B287417BE1B?sharingId=F3C1BA3926A133BD'
     },
     {
+        id: 4,
         certLogo: CSharp,
         certTitle: 'Foundational C# with Microsoft',
         certLink: 'https://www.freecodecamp.org/certification/fcca831935a-2d31-479a-bd5b-25f6d3913959/foundational-c-sharp-with-microsoft'
     },
     {
+        id: 5,
         certLogo: SERNStack,
         certTitle: 'SERN Stack Developer',
         certLink: 'https://stdntpartners-my.sharepoint.com/:i:/g/personal/brian_kemboi_studentambassadors_com/ET0y_4DQE8dPmevniCsnJrIBC3wNjK8k9cRiVCIReuBVGQ?e=ygKvaG'
     },
     {
+        id: 6,
         certLogo: MSAStudentTrainer,
         certTitle: 'Microsoft Learn Student Ambassadors Student Trainer',
         certLink: 'https://www.credly.com/badges/18f07a50-581f-4252-a288-f63bfa3180bd/linked_in_profile'
     },
     {
+        id: 7,
         certLogo: MSAStudentMentor,
         certTitle: 'Microsoft Learn Student Ambassadors Mentor',
         certLink: 'https://www.credly.com/badges/b97d71c1-957a-41c1-bc68-92925a0d162d/linked_in_profile'
     },
     {
+        id: 8,
         certLogo: MSAStudentMentee,
         certTitle: 'Microsoft Learn Student Ambassadors Mentee',
         certLink: 'https://www.credly.com/badges/833820ee-6731-4195-82a6-ecfeb02104a7/public_url'
     },
     {
+        id: 9,
         certLogo: AzureDBMySQLEmerald,
         certTitle: 'Azure Database for MySQL - Emerald Contributor',
         certLink: 'https://www.credly.com/badges/cbf32685-e02f-41f8-8a08-e7eee076234b/public_url'
     },
     {
+        id: 10,
         certLogo: AzureDBMySQLGold,
         certTitle: 'Azure Database for MySQL - Gold Contributor',
         certLink: 'https://www.credly.com/badges/13cf3216-54c3-48cf-b287-9b70597762dd/public_url'
