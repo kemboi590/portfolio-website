edb90e51905dd0dfeca5b5db661e64c190707d97
kemboi590 <bkemboi590@gmail.com>
2024-06-25 10:05:44 +0300

    height

--- src/pages/8.Projects/Projects.tsx
+++ src/pages/8.Projects/Projects.tsx
@@ -8,7 +8,7 @@ const Projects = () => {
                 {projectsData.map((project) => (
                     <div key={project.id} className='flex flex-col justify-center items-center bg-gray-800 w-full md:w-[30%] h-[500px] gap-5 cursor-pointer transform transition duration-300 hover:-translate-y-1 rounded-lg '>
                         <div className="w-full flex justify-center items-center " >
-                            <img src={project.projectLogo} alt={project.projectName} className='rounded-lg w-full px-2 h-[150px] object-cover' />
+                            <img src={project.projectLogo} alt={project.projectName} className='rounded-lg w-full px-2 h-[180px] md:h-[150px] object-cover' />
                         </div>
                         <div className='text-xl leading-8 p-4'>
                             <h3 className='flex items-center justify-center mb-4 text-[#00df9a] '>
